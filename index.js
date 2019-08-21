bbf78d22e49c20eaf6b1e3f74d2bf8b58e0cfef7
Tim Dill <brodt7258@gmail.com>
2019-08-21 08:27:47 -0400

    init, mvp.  all of it

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+
+const users = require('./data/usersQueries');
+
+const server = express();
+server.use(express.json());
+
+server.post('/api/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const hash = bcrypt.hashSync(password, 8);
+    const newUser = await users.addUser({ username, password: hash });
+
+    res.status(201).json(newUser);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+server.post('/api/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await users.getUserByName(username);
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({ cookie: user.id });
+    } else {
+      res.status(403).json({ message: 'You shall not pass!' });
+    }
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+server.get('/api/users', async (req, res) => {
+  try {
+    //console.log(req.headers.cookie);
+    const { cookie } = req.headers;
+    
+    if (await users.getUserById(cookie)) {
+      res.status(200).json(await users.getUsers());
+    } else {
+      res.status(403).json({ message: 'You shall not pass!' })
+    }
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+server.listen(5000, () => {
+  console.log('server on :5000');
+});
