aab054e5ce9d15c93227eebd03c10dd0be2a10c2
Tim Dill <brodt7258@gmail.com>
2019-08-21 22:36:07 -0400

    styled login

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
 import React, { useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
 
 import Login from './components/Login';
 import Register from './components/Register';
 import Users from './components/Users';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  html, body {
+    height: 100%
+  }
+
+  body {
+    background-image:
+      linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)),
+      radial-gradient(
+        circle at top,
+        #FF9933,
+        #cc007e
+      );
+  }
+`;
 
 function App() {
   const [user, setUser] = useState(null);
@@ -25,4 +47,11 @@ function App() {
   }
 }
 
-export default App;
+const Wrapper = () => (
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
+
+export default Wrapper;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,54 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Form = styled.form`
+  width: 40%;
+  margin: 5rem auto;
+  background-color: white;
+  padding: 2rem;
+  border-radius: 1rem;
+  box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > * {
+    margin: 0.75rem 0;
+  }
+
+  input {
+    padding: 0.5rem;
+    border: 1px solid grey;
+    border-radius: 0.5rem;
+    margin-left: 1rem;
+  }
+
+  .login {
+    padding: 0.5rem 2rem;
+    border-radius: 0.5rem;
+    border: none;
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.15);
+    color: white;
+    background-color: #cc007e;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #ff66c4
+    }
+  }
+
+  .register {
+    background: none;
+    border: none;
+    color: #cc007e;
+    cursor: pointer;
+
+    &:hover {
+      color: #ff66c4
+    }
+  }
+`;
 
 const Login = ({ setUser, setRegister }) => {
   const [username, setUsername] = useState('');
@@ -19,7 +68,8 @@ const Login = ({ setUser, setRegister }) => {
   };
 
   return (
-    <form onSubmit={e => sumbit(e, username, password)}>
+    <Form onSubmit={e => sumbit(e, username, password)}>
+      <h2>Login</h2>
       <div>
         <label htmlFor="">
           Username:
@@ -38,13 +88,19 @@ const Login = ({ setUser, setRegister }) => {
           onChange={e => setPassword(e.target.value)}
         />
       </div>
-      <button type="submit">
+      <button 
+        type="submit"
+        className="login"
+      >
         Login
       </button>
-      <button onClick={() => setRegister(true)}>
+      <button 
+        onClick={() => setRegister(true)}
+        className="register"
+      >
         Register New Account
       </button>
-    </form>
+    </Form>
   );
 };
 
