437f4d286c05704549a9fe28a65c0d538dcda8d1
Tim Dill <brodt7258@gmail.com>
2019-08-27 17:29:39 -0400

    Update for webauth 2 sessions

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+const session = require('express-session');
 
 const users = require('./data/usersQueries');
 
 const server = express();
 server.use(express.json());
 server.use(cors());
+server.use(session({
+  name: 'webauth',
+  secret: 'JetBeamsCantMeltSteelFuel',
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 60 * 30
+  }
+}));
 
-server.use('/restricted', async (req, res, next) => {
+const restricted = async (req, res, next) => {
   try {
-    const { cookie } = req.headers;
-
-    if (await users.getUserById(cookie)) {
+    if (req.session && req.session.user && await users.getUserById(req.session.user.id)) {
       next();
     } else {
-      res.status(403).json({ message: 'You shall not pass!' });
+      res.status(403).json({ message: 'invalid credentials' });
     }
   } catch(err) {
     res.status(500).json(err);
   }
-});
+};
 
 server.post('/api/register', async (req, res) => {
   try {
@@ -42,27 +51,28 @@ server.post('/api/login', async (req, res) => {
     const user = await users.getUserByName(username);
 
     if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(200).json({ cookie: user.id });
+      req.session.user = user;
+      res.status(200).json({ message: 'welcome' });
     } else {
       res.status(403).json({ message: 'You shall not pass!' });
     }
+  } catch(err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+server.get('/api/users', restricted, async (req, res) => {
+  try {
+    res.status(200).json(await users.getUsers());
   } catch(err) {
     res.status(500).json(err);
   }
 });
 
-server.get('/api/users', async (req, res) => {
-  try {
-    const { authorization } = req.headers;
-    
-    if (await users.getUserById(authorization)) {
-      res.status(200).json(await users.getUsers());
-    } else {
-      res.status(403).json({ message: 'You shall not pass!' });
-    }
-  } catch(err) {
-    res.status(500).json(err);
-  }
+server.get('/api/logout', restricted, async (req, res) => {
+  req.session.destroy();
+  res.status(204).end();
 });
 
 server.listen(5000, () => {
